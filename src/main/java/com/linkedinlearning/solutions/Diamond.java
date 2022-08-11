f56ae8b14e65f44462b65a790cd0f553393ff33e
Kevin Wittek <kiview@users.noreply.github.com>
2022-08-11 07:57:27 +0000

    Add Diamond

---
+++ src/main/java/com/linkedinlearning/solutions/Diamond.java
@@ -0,0 +1,63 @@
+package com.linkedinlearning.solutions;
+
+import java.lang.module.ModuleReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Diamond {
+  private static final char START_LETTER = 'A';
+  private char letter;
+
+  public Diamond(char letter) {
+    this.letter = letter;
+  }
+
+  public String renderDiamond() {
+
+    List<String> diamondRows = createRowsTillMiddle();
+    List<String> mirroredRows = mirrorRows(diamondRows);
+
+    diamondRows.addAll(mirroredRows);
+
+    return String.join("\n", diamondRows);
+
+  }
+
+  private List<String> createRowsTillMiddle() {
+    var rows = new ArrayList<String>();
+
+    int numberOfLetters = letter - START_LETTER;
+
+    for (int letterIndex = 0; letterIndex <= numberOfLetters; letterIndex++) {
+      rows.add(createRow(letterIndex));
+    }
+
+    return rows;
+  }
+
+  private String createRow(int letterIndex) {
+    char currentLetter = (char) (START_LETTER + letterIndex);
+
+    int paddingSize = this.letter - currentLetter;
+    int insidePaddingSize = Math.max(letterIndex * 2 - 1, 0);
+    String outerPadding = " ".repeat(paddingSize);
+    String innerPadding = " ".repeat(insidePaddingSize);
+
+    String row = outerPadding + currentLetter + innerPadding;
+    if (letterIndex > 0) {
+      row += currentLetter;
+    }
+    row += outerPadding;
+
+    return row;
+  }
+
+  private List<String> mirrorRows(List<String> rows) {
+    var mirroredRows = new ArrayList<String>(rows);
+    Collections.reverse(mirroredRows);
+    mirroredRows.remove(0);
+    return mirroredRows;
+  }
+
+}
---
+++ src/test/java/com/linkedinlearning/solutions/DiamondTest.java
@@ -0,0 +1,31 @@
+package com.linkedinlearning.solutions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DiamondTest {
+  @Test
+  void aDiamond() {
+    var aDiamond = new Diamond('A').renderDiamond();
+    var expectedADiamond = """
+        A""";
+    Assertions.assertEquals(expectedADiamond, aDiamond);
+  }
+
+  @Test
+  void dDiamond() {
+
+    var dDiamond = new Diamond('D').renderDiamond();
+    var expectedDDiamond = """
+           A  \s
+          B B \s
+         C   C\s
+        D     D
+         C   C\s
+          B B \s
+           A  \s""";
+    Assertions.assertEquals(expectedDDiamond, dDiamond);
+
+  }
+
+}
