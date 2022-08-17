ed539f694d0a1091398bece53aa723e67e8e5625
Kevin Wittek <kiview@users.noreply.github.com>
2022-08-17 11:29:09 +0200

    Add challenge package structure (#1)

---
+++ src/test/java/com/linkedinlearning/challenges/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.linkedinlearning.challenges;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+  @Test
+  void firstTest() {
+    Assertions.assertTrue(true);
+  }
+
+}
